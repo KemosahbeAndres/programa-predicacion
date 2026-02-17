c54eaa66c17447f631aaad14ef50843accd0f5b5
Andres Cubillos S. <andres.cubillos@epchinchorro.cl>
2026-02-17 10:23:44 -0300

    Routes updated

--- src/router/index.js
+++ src/router/index.js
@@ -1,19 +1,19 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
 
-import Programa    from '@/views/Programas.vue'
+import Programas    from '@/views/Programas.vue'
 import Territorios from '@/views/Territorios.vue'
 import Capitanes   from '@/views/Capitanes.vue'
 import Eventos     from '@/views/Eventos.vue'
 import Ajustes     from '@/views/Ajustes.vue'
 
 const routes = [
-  { path: '/',            redirect: '/programa' },
-  { path: '/programas',    name: 'Programas',    component: Programa },
+  { path: '/',            redirect: '/programas' },
+  { path: '/programas',    name: 'Programas',    component: Programas },
   { path: '/territorios', name: 'Territorios', component: Territorios },
   { path: '/capitanes',   name: 'Capitanes',   component: Capitanes },
   { path: '/eventos',     name: 'Eventos',     component: Eventos },
   { path: '/ajustes',     name: 'Ajustes',     component: Ajustes },
-  { path: '/:pathMatch(.*)*', redirect: '/programa' }
+  { path: '/:pathMatch(.*)*', redirect: '/programas' }
 ]
 
 
